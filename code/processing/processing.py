8eb470711499ca9aa9101fd4c310282d85db416d
KR Hariharan <krhariharan100@gmail.com>
2023-02-21 16:24:01 +0530

    Normalizing data and adding powers

--- code/processing/processing.py
+++ code/processing/processing.py
@@ -7,19 +7,33 @@ else:
     print("Enter file to process")
     inputFile = input()
 
-inputData = pd.read_csv("../../data/raw/"+inputFile, thousands=",")[{"Date", "Price"}]
+# reading input
+inputData = pd.read_csv("../../data/raw/"+inputFile, thousands=",")[["Date", "Price"]]
 inputData["Date"] = pd.to_datetime(inputData["Date"])
 
-inputData["Previous Price"] = inputData["Price"].shift(-1)
-inputData["Previous Date"] = inputData["Date"].shift(-1)
-inputData["Date Delta"] = inputData["Date"] - inputData["Previous Date"]
+# limits to normalize data
+minPrice = inputData["Price"].min()
+priceRange = inputData["Price"].max() - inputData["Price"].min()
 
+# Add missing dates (ie, holidays and weekends)
 d_range = pd.date_range(inputData["Date"].min(), inputData["Date"].max())
 inputData = inputData.set_index('Date').reindex(d_range)
 
+# Giving missing dates, price value that is the average of the previous and next biusiness day price
 inputData["Price"] = (inputData["Price"].fillna(method='ffill') + inputData["Price"].fillna(method='bfill'))/2
 
-inputData = inputData["Price"]
+# Normalizing price, and then adding powers
+inputData = (inputData[["Price"]] - minPrice)/ priceRange
+inputData["P^0.5"] = inputData["Price"] ** 0.5
+inputData["P^2"] = inputData["Price"] ** 2
+inputData["P^3"] = inputData["Price"] ** 3
+inputData["P^4"] = inputData["Price"] ** 4
 
-print(inputData)
+limits = pd.Series(data=[minPrice, priceRange], index=["minPrice", "priceRange"])
+
+if __debug__:
+    print(inputData)
+    print(limits)
+
 inputData.to_csv("../../data/processed/"+inputFile)
+limits.to_csv("../../data/processed/limits_"+inputFile)
