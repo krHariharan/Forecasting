2d4a77dbb6cba3a790b7dc336f857b4acf5c1d41
KR Hariharan <krhariharan100@gmail.com>
2023-02-28 08:35:01 +0530

    More robust param gen

--- code/processing/parameter_gen.py
+++ code/processing/parameter_gen.py
@@ -4,28 +4,35 @@ import math
 
 def power(factor: float):
     def p(t: float):
+        t = t % 1.0
         return t ** factor
     return p
 
-def partial_sin(factor: float):
+def partial_sin(factor: float, power: float, amplitude: float):
     def sin(t: float):
-        return math.sin(factor*t)
+        t1 = t % 1.0
+        t0 = t / amplitude
+        return (t0 ** power) * math.sin(factor*t1)
     return sin
 
-def partial_cos(factor: float):
+def partial_cos(factor: float, power: float, amplitude: float):
     def cos(t: float):
-        return math.cos(factor*t)
+        t1 = t % 1.0
+        t0 = t / amplitude
+        return (t0 ** power) * math.cos(factor*t1)
     return cos
 
 def beta_dist(a: float, b: float):
     denom = pow((a/(a+b)), a) * pow((b/(a+b)), b)
     def bd(t: float):
+        t = t % 1.0
         return pow(t,a) * pow(1-t,b) / denom
     return bd
 
 def sym_beta_dist(a: float, b: float, invert: int):
     denom = pow((a/(a+b)), a) * pow((b/(a+b)), b)
     def bd(t: float):
+        t = t % 1.0
         if t < 0.5:
             t0 = 2*t
             return pow(t0,a) * pow(1-t0,b) / denom
@@ -39,7 +46,8 @@ def parameter_gen(start_date: datetime, end_date: datetime, freq: int):
     d_range = pd.date_range(start_date, end_date)
     parameters = pd.DataFrame()
     parameters = parameters.reindex(d_range)
-    parameters["t"] = ((parameters.index - start_date).days.astype('float64') / float(freq)) % 1.0
+    parameters["t"] = ((parameters.index - start_date).days.astype('float64') / float(freq))
+    amplitude = parameters["t"].max()
     
     #polynomial parameters
     parameters[str(freq)+"_t"] = parameters["t"].map(power(1.0))
@@ -48,12 +56,13 @@ def parameter_gen(start_date: datetime, end_date: datetime, freq: int):
     parameters[str(freq)+"_t^3"] = parameters["t"].map(power(3))
     parameters[str(freq)+"_t^4"] = parameters["t"].map(power(4))
     # trigonometric parameters
-    parameters[str(freq)+"_sin"] = parameters["t"].map(partial_sin(2*math.pi))
-    parameters[str(freq)+"_cos"] = parameters["t"].map(partial_sin(2*math.pi))
-    parameters[str(freq)+"_half_sin"] = parameters["t"].map(partial_sin(math.pi))
-    parameters[str(freq)+"_half_cos"] = parameters["t"].map(partial_sin(2*math.pi))
-    parameters[str(freq)+"_quarter_sin"] = parameters["t"].map(partial_sin(math.pi/2))
-    parameters[str(freq)+"_quarter_cos"] = parameters["t"].map(partial_sin(math.pi/2))
+    for i in [0.0, 0.5, 1.0, 2.0, 3.0]:
+        parameters[str(freq)+"_t^"+str(i)+"_sin"] = parameters["t"].map(partial_sin(2*math.pi, i, amplitude))
+        parameters[str(freq)+"_t^"+str(i)+"_cos"] = parameters["t"].map(partial_sin(2*math.pi, i, amplitude))
+        parameters[str(freq)+"_t^"+str(i)+"_half_sin"] = parameters["t"].map(partial_sin(math.pi, i, amplitude))
+        parameters[str(freq)+"_t^"+str(i)+"_half_cos"] = parameters["t"].map(partial_sin(2*math.pi, i, amplitude))
+        parameters[str(freq)+"_t^"+str(i)+"_quarter_sin"] = parameters["t"].map(partial_sin(math.pi/2, i, amplitude))
+        parameters[str(freq)+"_t^"+str(i)+"_quarter_cos"] = parameters["t"].map(partial_sin(math.pi/2, i, amplitude))
     # beta distribution based
     for i in [0.5, 1, 2, 4, 8]:
         for j in [0.5, 1, 2, 4, 8]:
