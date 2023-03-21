35f8113668e64e67f1adcd670e98069494ce1b71
KR Hariharan <krhariharan100@gmail.com>
2023-03-21 18:23:09 +0530

    Added Support for generated params

--- code/vmd_lstm.py
+++ code/vmd_lstm.py
@@ -37,17 +37,20 @@ else:
 dataframe = pd.read_csv("../data/processed/"+inputFile, index_col=0)
 dataframe.index = pd.to_datetime(dataframe.index)
 
+params_full_period = parameter_gen(dataframe.index.min(), dataframe.index.max(), (dataframe.index.max() - dataframe.index.min()).days).reset_index(drop=True)
 params_10_yrs = parameter_gen(dataframe.index.min(), dataframe.index.max(), 3650).reset_index(drop=True)
-params_5_yrs = parameter_gen(dataframe.index.min(), dataframe.index.max(), 3650).reset_index(drop=True)
-params_2_yrs = parameter_gen(dataframe.index.min(), dataframe.index.max(), 3650).reset_index(drop=True)
+params_5_yrs = parameter_gen(dataframe.index.min(), dataframe.index.max(), 1825).reset_index(drop=True)
+params_2_yrs = parameter_gen(dataframe.index.min(), dataframe.index.max(), 730).reset_index(drop=True)
 annual_params = parameter_gen(dataframe.index.min(), dataframe.index.max(), 365).reset_index(drop=True)
 quarterly_params = parameter_gen(dataframe.index.min(), dataframe.index.max(), 121).reset_index(drop=True)
 monthly_params = parameter_gen(dataframe.index.min(), dataframe.index.max(), 30).reset_index(drop=True)
 weekly_params = parameter_gen(dataframe.index.min(), dataframe.index.max(), 7).reset_index(drop=True)
-generated_params = pd.concat([annual_params, quarterly_params, monthly_params, weekly_params], axis=1, join='inner')
+generated_params = pd.concat([params_full_period, params_10_yrs, params_5_yrs, params_2_yrs, annual_params, quarterly_params, monthly_params, weekly_params], axis=1, join='inner')
+# print(generated_params)
 
 datasetFull = dataframe.values
 datasetFull = datasetFull.astype('float32')
+# print(datasetFull)
 
 #. some sample parameters for VMD  
 alpha = 2000       # moderate bandwidth constraint  
@@ -62,9 +65,11 @@ tol = 1e-7
 u, u_hat, omega = VMD(datasetFull, alpha, tau, K, DC, init, tol)  
 
 look_back = 3
-train_size = int(len(datasetFull) * 0.67)
+additional_param_count = 0
+train_size = int(len(datasetFull) * 0.95)
 test_size = len(datasetFull) - train_size
 train, test = datasetFull[0:train_size,:], datasetFull[train_size:len(datasetFull),:]
+generated_train, generated_test = generated_params.iloc[0:train_size, :], generated_params.iloc[train_size:len(datasetFull), :]
 # reshape into X=t and Y=t+1
 _, trainYfull = create_dataset(train, look_back)
 _, testYfull = create_dataset(test, look_back)
@@ -72,7 +77,20 @@ _, testYfull = create_dataset(test, look_back)
 trainPredictFull = np.zeros(len(trainYfull))
 testPredictFull = np.zeros(len(testYfull))
 
-for dataset in u:
+all_params = generated_params
+all_params['u'] = pd.Series(datasetFull[:, 0])
+params_corr = all_params.corr()["u"].abs()
+selected_params = params_corr.nlargest(1+additional_param_count).index
+# print(params_corr.nlargest(1+additional_param_count))
+
+for i, dataset in enumerate(u):
+    print(i)
+    all_params = generated_params
+    all_params['u'] = pd.Series(dataset)
+    params_corr = all_params.corr()["u"].abs()
+    selected_params = params_corr.nlargest(1+additional_param_count).index
+    # print(params_corr.nlargest(1+additional_param_count))
+
     dataset = np.reshape(dataset, (-1,1))
     # normalize the dataset
     scaler = MinMaxScaler(feature_range=(0, 1))
@@ -80,17 +98,20 @@ for dataset in u:
     # split into train and test sets
     train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
     # reshape into X=t and Y=t+1
-    trainX, trainY = create_dataset(train, look_back)
-    testX, testY = create_dataset(test, look_back)
+    trainX, trainY = create_dataset(train, look_back, generated_train[selected_params[1:]])
+    testX, testY = create_dataset(test, look_back, generated_test[selected_params[1:]])
     # reshape input to be [samples, time steps, features]
     trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+    # for i in range( trainX.shape[0]):
+    #     print(trainX[i])
+    #     print(trainY[i])
     testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
     # create and fit the LSTM network
     model = Sequential()
-    model.add(LSTM(4, input_shape=(1, look_back)))
+    model.add(LSTM(4, input_shape=(1, look_back+additional_param_count)))
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(trainX, trainY, epochs=10, batch_size=1, verbose=2)
+    model.fit(trainX, trainY, epochs=10, batch_size=1, verbose=0)
     # make predictions
     trainPredict = model.predict(trainX)
     testPredict = model.predict(testX)
@@ -128,4 +149,4 @@ testPredictPlot[len(trainPredictFull)+(look_back*2)+1:len(dataset)-1, :] = testP
 plt.plot(scaler.inverse_transform(dataset))
 plt.plot(trainPredictPlot)
 plt.plot(testPredictPlot)
-plt.savefig("simple_lstm.png")
+plt.savefig("vmd_lstm.png")
